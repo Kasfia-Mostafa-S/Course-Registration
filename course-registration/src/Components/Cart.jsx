4cd93a6d38747b2b375e69876d5e0113b36e77aa
SwornaCode <kasfiamostafa03@gmail.com>
2023-09-14 23:22:19 +0600

    select button and fixed card information

--- course-registration/src/Components/Cart.jsx
+++ course-registration/src/Components/Cart.jsx
@@ -1,50 +1,57 @@
-/* eslint-disable react/jsx-key */
-import React, { useEffect, useState } from 'react';
-import { BiDollar  } from 'react-icons/bi';
-import { BsBook  } from 'react-icons/bs';
+import React, { useEffect, useState } from "react";
+import { BiDollar } from "react-icons/bi";
+import { BsBook } from "react-icons/bs";
 
 const Cart = () => {
+  const [courses, setCourses] = useState([]);
 
-const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("./course.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
 
-useEffect(()=>{
-  fetch('./course.json')
-  .then(res => res.json())
-  .then(data => setCourses(data))
-},[])
-
-
-// created card and cart
+  // created card and cart
 
   return (
     <div>
-      <div className='flex m-10 pl-10 gap-10'>
-          <div className='grid grid-cols-3 gap-5'>
-            {
-              courses.map(course => (
-                <div key={course.id} className='card-container h-96 w-72 bg-white rounded-lg'>
-                <img className='p-2' src="https://i.ibb.co/fYtPmDv/Rectangle-2-6.png" alt="" />
-                  <h3 className='mt-2 text-semibold text-3xl'>title</h3>
-                  <p className='text-gray-400 mt-2'>description</p>
-    
-                  <div className='flex justify-between text-gray-600'>
-                    <div className='flex items-center gap-1'>
-                      <p className='text-2xl'><BiDollar ></BiDollar ></p>
-                      <p className='text-xl'>Price:</p>
-                    </div>
-                       <div className='flex items-center gap-1'>
-                       <p className='text-2xl'><BsBook></BsBook></p>
-                       <p className='text-xl'>Credit:hr</p>
-                       </div>
-                  </div>
-               </div>
-              ))
-            }
-          </div>
-           
-           <div>
-           <h1>this is cart</h1>
-           </div>
+      <div className="flex m-10 pl-10 gap-10">
+        <div className="grid grid-cols-3 gap-5">
+          {courses.map((course) => (
+            <div
+              key={course.id}
+              className="card-container h-full w-80 bg-white rounded-lg"
+            >
+              <img className="p-5" src={course.img_url} alt="" />
+              <h3 className="m-2 text-bold text-xl">{course.title}</h3>
+              <p className="text-gray-400 m-2 text-sm">{course.description}</p>
+
+              <div className="flex justify-between text-gray-600 p-4">
+                <div className="flex items-center gap-1">
+                  <p className="text-2xl">
+                    <BiDollar></BiDollar>
+                  </p>
+                  <p className="text-xl ">Price: {course.price}</p>
+                </div>
+                <div className="flex items-center gap-1">
+                  <p className="text-2xl">
+                    <BsBook></BsBook>
+                  </p>
+                  <p className="text-xl">Credit: {course.credit_hour}hr</p>
+                </div>
+              </div>
+
+              <div className="bg-blue-500 m-2 text-white text-center rounded-md h-8 flex items-center justify-center">
+                <button>Select</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <h1>this is cart</h1>
+        </div>
+        
       </div>
     </div>
   );
