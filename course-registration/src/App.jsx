ab2d7136d7953b1d1437e5e066de14c0896ee9e4
SwornaCode <kasfiamostafa03@gmail.com>
2023-09-14 20:44:57 +0600

    Created react file with fake data of courses

---
+++ course-registration/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-center font-bold text-4xl mt-5'>Course Registration</h1>
+      
+    </>
+  )
+}
+
+export default App
